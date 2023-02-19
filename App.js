0a65424d09f00fc9e02e0473c4a1f9e93c0fe125
aahalani <aahalani@gmail.com>
2023-02-19 14:30:06 +0530

    splash

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/trans.png')} style={styles.logo} />
+      <Image source={require('./assets/picture.png')} style={styles.logo1} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>  
+        <Image source={require('./assets/google.png')} style={styles.google} />
+          <Text style={styles.buttonText}>Login with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button1}>
+          <Text style={styles.buttonText1}>Sign in with email address</Text>
+        </TouchableOpacity>
+        <View style={styles.line}>
+        </View>
+        <View style={styles.bottom}>
+        <Text>Already have an account?</Text>
+        <TouchableOpacity>
+        <Text style={styles.buttonT}> Sign in!</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    paddingTop: 50,
   },
+  logo: {
+    width: 330,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  logo1: {
+    width: 500,
+    height: 270,
+    resizeMode: 'contain',
+  },
+  google: {
+    resizeMode: 'contain',
+    marginLeft:13,
+  },
+  line: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 4,
+    width: '100%',
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  bottom:{
+    flexDirection:'row',
+  },
+  button: {
+    backgroundColor: '#9E2D0B',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    margin: 10,
+    borderRadius: 15,
+    width:292,
+    height:54,
+  },
+  button1: {
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 15,
+    padding: 10,
+    margin: 10,
+    width:292,
+    height:54,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  
+  buttonText1: {
+    color: '#000',
+    fontSize: 16,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft:35,
+  },
+  buttonT:{
+    color:"#9E2D0B"
+  }
 });
+
+export default App;
